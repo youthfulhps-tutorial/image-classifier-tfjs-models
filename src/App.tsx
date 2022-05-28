35a29a1cebe2a88ab1b8a6a64ecb048ea8471e96
youthfulhps <ybh942002@gmail.com>
2022-05-28 19:21:17 +0900

    Applied custom hooks and components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import Webcam from "./components/Webcam";
+import Figure from "./components/Figure";
+import useNet from "./hooks/useNet";
+import useWebcam from "./hooks/useWebcam";
+import styled from "styled-components";
+import DotLoader from "./components/Common/DotLoader";
 
-function App() {
+const StyledApp = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: #3d403f;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  & > * {
+    border-radius: 8px;
+    -webkit-box-shadow: 1px 5px 20px 0px #000000;
+    box-shadow: 1px 5px 20px 0px #000000;
+  }
+`;
+
+const App = () => {
+  const { cameraRef, runWebcam, isLoading } = useWebcam();
+  const { classifyTarget, result } = useNet(cameraRef);
+
+  useEffect(() => {
+    runWebcam();
+    classifyTarget();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StyledApp>
+      {isLoading ? (
+        <DotLoader />
+      ) : (
+        <>
+          <Webcam cameraRef={cameraRef} />
+          <Figure {...result} />
+        </>
+      )}
+    </StyledApp>
   );
-}
+};
 
 export default App;
